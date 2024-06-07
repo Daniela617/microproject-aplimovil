f2ffd0661ed12fee12798983d5000b431a704faf
Daniela <driascos@unicauca.edu.co>
2024-06-06 20:27:42 -0500

    Add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from 'src/environments/environment.development';
+import {provideFirestore, getFirestore} from '@angular/fire/firestore'
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(()=> initializeApp(environment.firebase)),
+    provideFirestore(()=> getFirestore())
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyARtLmwKlQ6v7iQGOT_eUcxORDgTnHq7ZM",
+    authDomain: "microproject-apliweb.firebaseapp.com",
+    projectId: "microproject-apliweb",
+    storageBucket: "microproject-apliweb.appspot.com",
+    messagingSenderId: "71079688966",
+    appId: "1:71079688966:web:a82481284bfcb6b365bc87"
+  }
+};
